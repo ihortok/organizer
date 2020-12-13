5483b02b551c6fcb2696fe230d54cd44559b505b
ihortok <itokaryk@gmail.com>
2020-12-13 15:15:19 +0200

    new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,22 @@
 class TasksController < ApplicationController
   get '/tasks' do
-    @tasks = Task.all
+    @tasks = current_user.tasks
     slim :'tasks/index.html'
+  end
+
+  get '/tasks/new' do
+    redirect '/login' unless logged_in?
+
+    slim :'/tasks/new.html'
+  end
+
+  post '/tasks/create' do
+    @task = Task.new(params.merge({ user_id: current_user.id }))
+
+    if @task.save
+      redirect '/tasks'
+    else
+      slim :'/tasks/new.html', locals: { message: 'Something went wrong. Please try again.' }
+    end
   end
 end
